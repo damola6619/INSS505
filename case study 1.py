c467b40070a837ba674d2a797e9f29931b800ba3
damola6619 <oluwafemia0919@bowiestate.students.edu>
2023-11-02 23:14:07 -0500

    lab1

---
+++ case study 1.py
@@ -0,0 +1,9 @@
+#collecting two inputs from users using the input function
+length=int(input("Enter the length of a rectangle:"))
+breadth=int(input("Enter the breadth of a rectangle:"))
+
+#formula to calculate the area of a rectangle is
+#Area = length*breath
+
+Area=(length*breadth)
+print(Area)
