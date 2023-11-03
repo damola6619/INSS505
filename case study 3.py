a2ebd93d7fe5e64879bbc5273507ea542af51710
damola6619 <oluwafemia0919@bowiestate.students.edu>
2023-11-03 00:24:08 -0500

    lab1

---
+++ case study 3.py
@@ -0,0 +1,13 @@
+number1 = int(input("enter number 1:"))
+number2 = int(input("enter number 2:"))
+number3 = int(input("enter number 3:"))
+number4 = int(input("enter number 4:"))
+number5 = int(input("enter number 5:"))
+
+sum = (number1+number2+number3+number4+number5)
+print(sum)
+
+
+average = sum/5
+print(int(average))
+print(type(average))
