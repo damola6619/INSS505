e3355d951d8e9c06ebad9ecd7a4b7e688b09f80b
damola6619 <oluwafemia0919@bowiestate.students.edu>
2023-11-03 01:01:21 -0500

    lab1

---
+++ case study 4.py
@@ -0,0 +1,13 @@
+# assign a value to the variable n
+n= 12
+#iteration statement
+for count in range(1,10):
+    n=n*count
+    print(n)
+
+"""n=12*count(1,10)
+n=12*1=12
+n=12
+n=12*2=24
+n=24,,,,
+"""
